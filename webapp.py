f2d5b919111ff616eecae617cd80903f87f4dbea
OwenABC1 <owennsullivan@icoud.com>
2024-09-27 13:40:12 -0700

    visual publish

--- webapp.py
+++ webapp.py
@@ -1,1 +1,11 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_index():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=False)
 
