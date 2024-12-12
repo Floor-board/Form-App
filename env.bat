de2ccfb40c8d72e9aa1a8f8e8ceaa6af2ff28b75
Floor-board <Sterling34@duck.com>
2024-12-12 10:44:54 -0800

    Delete env.bat

--- env.bat
+++
@@ -1,5 +0,0 @@
-set SECRET_KEY=Jaden
-set MONGO_CONNECTION_STRING=mongodb+srv://Gio:Giovanni.800@cluster0.r3rah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-set MONGO_DBNAME=database1
-set GITHUB_CLIENT_ID=Ov23liTCkOrzhhRNPPpO
-set GITHUB_CLIENT_SECRET=b93799e75c0f88ea9414efdb49fc4abf74d3d325
