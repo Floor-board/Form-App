45917a5cf15cc750c4e142af68f111ed662bffe0
JadenMolwane <jkmmkj528@gmail.com>
2025-01-15 13:37:37 -0800

    Delete env.bat

--- env.bat
+++
@@ -1,5 +0,0 @@
-set SECRET_KEY=Jaden
-set MONGO_CONNECTION_STRING=mongodb+srv://Gio:Giovanni.800@cluster0.r3rah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-set MONGO_DBNAME=database1
-set GITHUB_CLIENT_ID=Ov23liTCkOrzhhRNPPpO
-set GITHUB_CLIENT_SECRET=13ebc221e1ed70328c35cf00924f711ab481521e
